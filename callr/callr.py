1f321593894b5af7dbbf5d005ce38ea68c444dcb
Dion MITCHELL <dion.mitchell@callr.com>
2016-11-03 11:38:24 +0100

    feat(python version): update lib to be compatible with python 2 and 3, now uses future library

--- callr/callr.py
+++ callr/callr.py
@@ -1,106 +1,130 @@
+from future import standard_library
+standard_library.install_aliases()
+from past.builtins import basestring
+from builtins import object
+import sys
 ###
 # CALLR webservice communication library
 ###
 
 import json
-import urllib2
-from base64 import encodestring
+import urllib.request, urllib.error, urllib.parse
+import base64
 from random import randint
 
+PY3 = sys.version_info[0] >= 3
+
+def base64encode(bytes_or_str):
+    if PY3 and isinstance(bytes_or_str, str):
+        input_bytes = bytes_or_str.encode('utf8')
+    else:
+        input_bytes = bytes_or_str
+
+    output_bytes = base64.b64encode(input_bytes)
+    if PY3:
+        return output_bytes.decode('ascii')
+    else:
+        return output_bytes
+
 class Api(object):
-	_login = False
-	_password = False
-	_headers = {
-		"Expect": "",
-		"Content-Type": "application/json-rpc; charset=utf-8"
-	}
+    _login = False
+    _password = False
+    _headers = {
+        "Expect": "",
+        "Content-Type": "application/json-rpc; charset=utf-8"
+    }
 
-	API_URL = "https://api.thecallr.com/"
+    API_URL = "https://api.thecallr.com/"
 
-	###
-	# Initialization
-	# @param string login
-	# @param string password
-	###
-	def __init__(self, login, password, options = None):
-		self._login = login
-		self._password = password
-		self.set_options(options)
+    ###
+    # Initialization
+    # @param string login
+    # @param string password
+    ###
+    def __init__(self, login, password, options = None):
+        self._login = login
+        self._password = password
+        self.set_options(options)
 
-	def set_options(self, options):
-		if isinstance(options, dict):
-			if options.has_key("proxy"):
-				self.set_proxy(options["proxy"])
+    def set_options(self, options):
+        if isinstance(options, dict):
+            if "proxy" in options:
+                self.set_proxy(options["proxy"])
 
-	def set_proxy(self, proxy):
-		if isinstance(proxy, basestring):
-			proxy_handler = urllib2.ProxyHandler({'https': proxy})
-			opener = urllib2.build_opener(proxy_handler)
-			urllib2.install_opener(opener)
-		else:
-			raise CallrLocalException("PROXY_NOT_STRING", 1)
+    def set_proxy(self, proxy):
+        if isinstance(proxy, basestring):
+            proxy_handler = urllib.request.ProxyHandler({'https': proxy})
+            opener = urllib.request.build_opener(proxy_handler)
+            urllib.request.install_opener(opener)
+        else:
+            raise CallrLocalException("PROXY_NOT_STRING", 1)
 
-	###
-	# Send a request to CALLR webservice
-	###
-	def call(self, method, *params):
-		return self.send(method, params)
+    ###
+    # Send a request to CALLR webservice
+    ###
+    def call(self, method, *params):
+        return self.send(method, params)
 
-	###
-	# Send a request to CALLR webservice
-	###
-	def send(self, method, params = [], id = None):
-		self._check_auth()
+    ###
+    # Send a request to CALLR webservice
+    ###
+    def send(self, method, params = [], id = None):
+        self._check_auth()
 
-		json_data = json.dumps({
-			"id": (id, randint(100, 999))[id is None],
-			"jsonrpc": "2.0",
-			"method": method,
-			"params": params
-		})
+        json_data = json.dumps({
+            "id": (id, randint(100, 999))[id is None],
+            "jsonrpc": "2.0",
+            "method": method,
+            "params": params
+        }).encode('utf8')
 
-		self._headers["Authorization"] = "Basic %s" % encodestring('%s:%s' % (self._login, self._password)).replace('\n', '')
+        self._headers["Authorization"] = "Basic %s" % base64encode('%s:%s' % (self._login, self._password)).replace('\n', '')
 
-		req = urllib2.Request(self.API_URL, json_data, self._headers)
-		try:
-			res = urllib2.urlopen(req)
-			if res.code != 200:
-				raise CallrException("HTTP_CODE_ERROR", -1, {"http_code": res.code, "http_message": res.msg})
-			return self._parse_response(res.read())
-		except urllib2.HTTPError as e:
-			raise CallrException("HTTP_CODE_ERROR", -1, {"http_code": e.code, "http_message": e.msg})
-		except urllib2.URLError as e:
-			raise CallrException("HTTP_EXCEPTION", -2, {"exception": e})
+        req = urllib.request.Request(self.API_URL, json_data, self._headers)
+        try:
+            res = urllib.request.urlopen(req)
+            if res.code != 200:
+                raise CallrException("HTTP_CODE_ERROR", -1, {"http_code": res.code, "http_message": res.msg})
+            return self._parse_response(res.read())
+        except urllib.error.HTTPError as e:
+            raise CallrException("HTTP_CODE_ERROR", -1, {"http_code": e.code, "http_message": e.msg})
+        except urllib.error.URLError as e:
+            raise CallrException("HTTP_EXCEPTION", -2, {"exception": e})
 
 
-	def _check_auth(self):
-		if self._login == False or len(self._login) == 0 or self._password == False or len(self._password) == 0:
-			raise CallrLocalException("CREDENTIALS_NOT_SET", 1)
+    def _check_auth(self):
+        if self._login is None or len(self._login) == 0 or self._password is None or len(self._password) == 0:
+            raise CallrLocalException("CREDENTIALS_NOT_SET", 1)
 
-	###
-	# Response analysis
-	###
-	def _parse_response(self, body):
-		try:
-			data = json.loads(body)
-			if data and data.has_key("result") and data["result"]:
-				return data["result"]
-			elif data and data.has_key("error") and data["error"]:
-				raise CallrException(data["error"]["message"], data["error"]["code"])
-			else:
-				raise CallrException("INVALID_RESPONSE", -2, {"response": body})
-		except ValueError as e:
-			raise CallrException("INVALID_RESPONSE", -2, {"response": body})
+    ###
+    # Response analysis
+    ###
+    def _parse_response(self, body):
+        try:
+            if isinstance(body, str):
+                data = json.loads(body)
+            else: 
+                data = json.loads(str(body, 'utf-8'))				
+
+            if data and "result" in data:
+                return data["result"]
+            elif data and "error" in data and data["error"]:
+                raise CallrException(data["error"]["message"], data["error"]["code"])
+            else:
+                raise CallrException("INVALID_RESPONSE", -2, {"response": body})
+        except ValueError as e:
+            print(e)
+            raise CallrException("INVALID_RESPONSE", -2, {"response": body})
 
 
 ###
 # Exceptions
 ###
 class CallrException(Exception):
-	def __init__(self, msg, code = 0, data = None):
-		self.msg = msg
-		self.code = code
-		self.data = data
+    def __init__(self, msg, code = 0, data = None):
+        self.msg = msg
+        self.code = code
+        self.data = data
 
 class CallrLocalException(CallrException):
-	pass
+    pass
--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from distutils.core import setup
+import sys
 
 setup(
     name='callr',
-    version='1.0.1',
-    author='Michael Jacquin',
-    author_email='mj@callr.com',
+    version='2.0.1',
+    author='Michael Jacquin, Dion MITCHELL',
+    author_email='michael.jacquin@callr.com, dion.mitchell@callr.com',
     packages=['callr'],
     scripts=[],
     url='https://github.com/THECALLR/sdk-python',
     license='MIT',
-    description='Python SDK for CALLR API',
-    long_description="",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+    ],
+    description='Python>=2.7, SDK for CALLR API',
 )
